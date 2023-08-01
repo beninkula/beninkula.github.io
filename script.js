29eed2153e57ecf5ebcb58f6f8b4490b2dddf124
Beni <x22511016@student.ncirl.ie>
2023-08-01 21:42:17 +0100

    commit 1

---
+++ script.js
@@ -0,0 +1,26 @@
+﻿// Start of JavaScript by Beni Nkula
+function validateForm() {
+  let fname = document.forms["ctnform"]["fname"].value;
+  let surname = document.forms["ctnform"]["surname"].value;
+  let phoneno = document.forms["ctnform"]["phoneno"].value;
+  let email = document.forms["ctnform"]["email"].value;
+
+  if (fname.trim() === "") {
+    alert("First Name must be filled out");
+    return false;
+  }
+
+  if (surname.trim() === "") {
+    alert("Surname must be filled out");
+    return false;
+  }
+
+  if (phoneno.trim() === "") {
+    alert("Phone Number must be filled out");
+    return false;
+  }
+
+  if (email.trim() === "") {
+    alert("E-mail must be filled out");
+    return false;
+  }
